5ccd0617073038f20f0d185a67aac1da5b6e1696
“[Mishoro <[mishoroabose@gmail.com]>
2021-10-28 12:48:53 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
+import Nav from "./Nav/Nav";
+import Main from "./Main/Main";
+import SearchBar from "./SearchBar";
+
+function App() {
+  const [searchText, setSearchText] = useState("");
+  const [show, setShow] = useState(false);
+  const [movieData, setMovieData] = useState({});
+
+  const searchMovie = async () => {
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_KEY}&t=${searchText}`
+    );
+    const data = await response.json();
+    setMovieData(data);
+    setShow(true);
+  };
+
+  return (
+    <div className="App">
+      {/* <SearchBar/> */}
+      <Nav searchMovie={searchMovie} setSearchText={setSearchText} />
+      <Main setShow={setShow} show={show} movieData={movieData} />
+      
+    </div>
+  );
+}
+
+export default App;
